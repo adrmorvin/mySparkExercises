0adc47703c9467c2c9b80f40081ac9ad67836f54
adrmorvin <adriymoreno@gmail.com>
2022-03-02 09:40:41 +0100

    changes

--- src/main/scala/join1.scala
+++ src/main/scala/join1.scala
@@ -7,12 +7,12 @@ object join1 extends App {
   val customDF: DataFrame = MainRead.readCsvHeader("src/main/resources/retail_db/customers")
   val ordersDF: DataFrame = sparkSession.read.csv("src/main/resources/retail_db/orders")
 
-  val joinSol = customDF.filter(col("customer_fname").startsWith("M"))
-    .join(ordersDF, customDF.col("customer_id") === ordersDF.col("_c2"), "inner" )
-    .groupBy(col("customer_id"),col("customer_fname"),col("customer_lname")).count().where(col("count").gt(5))
-    .orderBy(desc("count"))
+  val joinSol = ordersDF.groupBy(col("_c2")).count().where(col("count").gt(5))
+    .join(customDF, ordersDF.col("_c2") === customDF.col("customer_id") , "inner" )
     .select("customer_fname","customer_lname", "count" )
+    .filter(col("customer_fname").startsWith("M"))
+    .orderBy(desc("count"))
   joinSol.show()
 
-  //joinSol.write.mode(SaveMode.Overwrite).option("codec", "org.apache.hadoop.io.compress.GzipCodec").option("sep", "|").csv("dataset/q7/solution")
+  joinSol.write.mode(SaveMode.Overwrite).option("codec", "org.apache.hadoop.io.compress.GzipCodec").option("sep", "|").csv("dataset/q7/solution")
 }
